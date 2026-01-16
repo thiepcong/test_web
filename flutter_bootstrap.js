f4c2544bd1ad10a19aa801f9d742d99bca0e8604
thiepcong <thiepcong12@gmail.com>
2026-01-16 17:23:04 +0700

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4222344414"
+    serviceWorkerVersion: "2605624746"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "af2787863fa3f4bbbdb7b02097895731",
+const RESOURCES = {"flutter_bootstrap.js": "bccd682fede6e667cec9dbd028e875dd",
 "version.json": "611dc8efba3840e925eb4ca7ba4b0e90",
 "splash/img/light-background.png": "f49c6be896de70f42d5a9c5b60305f3c",
 "splash/img/dark-background.png": "f49c6be896de70f42d5a9c5b60305f3c",
-"index.html": "a6a91c75be286bdbb39ea28f2d38ac5a",
-"/": "a6a91c75be286bdbb39ea28f2d38ac5a",
-"main.dart.js": "722ef5b2d4bd4d2f9a244392b976262f",
+"index.html": "b287cc1535ab2ed380921a4d002522d5",
+"/": "b287cc1535ab2ed380921a4d002522d5",
+"main.dart.js": "60fd5efccff33bfb1b7800272262df9f",
 "Web.config": "51998a27bbd7487b093c618da04bbb80",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "0eb7727eefc555dbee0d178b376babec",
