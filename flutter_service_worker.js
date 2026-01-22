8a3caca3264d3b9e14c6d4773a99bebbab53939b
thiepcong <thiepcong12@gmail.com>
2026-01-22 15:33:04 +0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c3d4be33ef99a7c04f2fadd78668bf0a",
+const RESOURCES = {"flutter_bootstrap.js": "0d8bed768aeefc7dba52240c6c11d0a5",
 "version.json": "611dc8efba3840e925eb4ca7ba4b0e90",
 "splash/img/light-background.png": "f49c6be896de70f42d5a9c5b60305f3c",
 "splash/img/dark-background.png": "f49c6be896de70f42d5a9c5b60305f3c",
 "index.html": "b287cc1535ab2ed380921a4d002522d5",
 "/": "b287cc1535ab2ed380921a4d002522d5",
-"main.dart.js": "e874813bcf09eac4a3a7cbbc93f4c670",
+"main.dart.js": "89ee857058c31745c35f456f6e8535cf",
 "Web.config": "51998a27bbd7487b093c618da04bbb80",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "0eb7727eefc555dbee0d178b376babec",
